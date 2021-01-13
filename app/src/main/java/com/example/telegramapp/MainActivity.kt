d43065a0de4d39fcc48baaa8131b02c092370165
Artistle <godofwarhijik132>
2021-01-13 19:22:11 +0300

    add realtime database || add change settings || refactoring

--- app/src/main/java/com/example/telegramapp/MainActivity.kt
+++ app/src/main/java/com/example/telegramapp/MainActivity.kt
@@ -1,27 +1,24 @@
 package com.example.telegramapp
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.Toolbar
+import com.example.telegramapp.activityes.RegisterActivity
 import com.example.telegramapp.databinding.ActivityMainBinding
-import com.example.telegramapp.ui.ChatsFragment
-import com.example.telegramapp.ui.SettingsFragment
-import com.mikepenz.materialdrawer.AccountHeader
-import com.mikepenz.materialdrawer.AccountHeaderBuilder
-import com.mikepenz.materialdrawer.Drawer
-import com.mikepenz.materialdrawer.DrawerBuilder
-import com.mikepenz.materialdrawer.model.DividerDrawerItem
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
-import com.mikepenz.materialdrawer.model.ProfileDrawerItem
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+import com.example.telegramapp.models.User
+import com.example.telegramapp.ui.fragments.ChatsFragment
+import com.example.telegramapp.ui.objects.AppDrawer
+import com.example.telegramapp.utilits.*
+import com.theartofdev.edmodo.cropper.CropImage
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
-    private lateinit var mDrawer: Drawer
-    private lateinit var mHeader: AccountHeader
+    lateinit var appDrawer:AppDrawer
     private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,116 +29,76 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        APP_ACTIVITY = this
         initFields()
         initFunction()
     }
 
     private fun initFunction() {
-        setSupportActionBar(mToolbar)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.data_container,ChatsFragment()).commit()
-        createHeader()
-        createDrawer()
+
+        if(AUTH.currentUser != null){
+            setSupportActionBar(mToolbar)
+            appDrawer.create()
+            replaceFragment(ChatsFragment(),false)
+        }else{
+            replaceActivity(RegisterActivity())
+        }
+
+
+//        createHeader()
+//        createDrawer()
     }
 
-    private fun createDrawer() {
-        mDrawer = DrawerBuilder()
-            .withActivity(this)
-            .withToolbar(mToolbar)
-            .withActionBarDrawerToggle(true)
-            .withSelectedItem(-1)
-            .withAccountHeader(mHeader)
-            .addDrawerItems(
-                PrimaryDrawerItem()
-                    .withIdentifier(100)
-                    .withIconTintingEnabled(true)
-                    .withName("Создать группу")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_create_groups),
-                PrimaryDrawerItem()
-                    .withIdentifier(101)
-                    .withIconTintingEnabled(true)
-                    .withName("Создать секретный чат")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_secret_chat),
-                PrimaryDrawerItem()
-                    .withIdentifier(102)
-                    .withIconTintingEnabled(true)
-                    .withName("Создать канал")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_create_channel),
-                PrimaryDrawerItem()
-                    .withIdentifier(103)
-                    .withIconTintingEnabled(true)
-                    .withName("Контакты")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_contacts),
-                PrimaryDrawerItem()
-                    .withIdentifier(104)
-                    .withIconTintingEnabled(true)
-                    .withName("Звонки")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_phone),
-                PrimaryDrawerItem()
-                    .withIdentifier(105)
-                    .withIconTintingEnabled(true)
-                    .withName("Избранное")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_favorites),
-                PrimaryDrawerItem()
-                    .withIdentifier(106)
-                    .withIconTintingEnabled(true)
-                    .withName("Настройки")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_settings),
-                DividerDrawerItem(),
-                PrimaryDrawerItem()
-                    .withIdentifier(107)
-                    .withIconTintingEnabled(true)
-                    .withName("Пригласить друзей")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_invate),
-                PrimaryDrawerItem()
-                    .withIdentifier(108)
-                    .withIconTintingEnabled(true)
-                    .withName("Вопросы о телеграмм")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_help)
-            )
-            .withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
-                override fun onItemClick(
-                    view: View?,
-                    position: Int,
-                    drawerItem: IDrawerItem<*>
-                ): Boolean {
 
-                    when(position){
-                        7 -> {
-                            supportFragmentManager.beginTransaction()
-                                .addToBackStack(null)
-                                .replace(R.id.data_container,SettingsFragment()).commit()
-                        }
-                    }
-
-                    return false
-                }
-            })
-            .build()
-    }
-
-    private fun createHeader() {
-        mHeader = AccountHeaderBuilder()
-            .withActivity(this)
-            .withHeaderBackground(R.drawable.header)
-            .addProfiles(
-                ProfileDrawerItem()
-                    .withName("name")
-                    .withEmail("mail@mail.ru")
-            ).build()
-    }
 
     private fun initFields() {
         mToolbar = mBinding.mainToolbar
+        appDrawer = AppDrawer(this,mToolbar)
+        initFirebase()
+        initUser()
+    }
 
+    private fun initUser() {
+        REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID)
+            .addListenerForSingleValueEvent(AppValueEventListener{
+                USER = it.getValue(USER::class.java) ?: User()
+            })
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE
+            && resultCode == Activity.RESULT_OK
+            && data != null){
+            val uri = CropImage.getActivityResult(data).uri
+            val path = REF_STORAGE_ROOT
+                .child(FOLDER_PROFILE_IMAGE)
+                .child(CURRENT_UID)
+            path.putFile(uri).addOnCompleteListener {task ->
+                if(task.isSuccessful){
+                    path.downloadUrl.addOnCompleteListener {task2 ->
+                        if(task2.isSuccessful){
+                            val photoUrl = task2.result.toString()
+                            REF_DATABASE_ROOT
+                                .child(NODE_USERS)
+                                .child(CURRENT_UID)
+                                .child(CHILD_PHOTO_URL)
+                                .setValue(photoUrl)
+                                .addOnCompleteListener {
+                                    if(it.isSuccessful){
+                                      showToast(getString(R.string.toast_data_update))
+                                        USER.photoUrl = photoUrl
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    fun hideKeyboard(){
+        val imm:InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(window.decorView.windowToken,0)
     }
 }
